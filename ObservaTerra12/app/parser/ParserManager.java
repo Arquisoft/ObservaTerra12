bb353e7886d5e630ffbea475961a3fa99d350922
Pablo-Casia <casia85@gmail.com>
2014-05-01 22:43:32 +0200

    Solucion conflicto

--- ObservaTerra12/app/parser/ParserManager.java
+++ ObservaTerra12/app/parser/ParserManager.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class ParserManager {
 
 	private final String FICH_PROP = "public/crawler/parser.properties";
-	
+
 	private BufferedReader in;
 
 	public ParserManager() {
@@ -25,25 +25,27 @@ public class ParserManager {
 	}
 
 	public void execute() {
-		String line;
-		try {
-			line = in.readLine(); //Se consume la linea de ejemplo
-			while ((line = in.readLine()) != null) {
-				String[] flags = line.trim().split("@");
-				Parser p = ParserFactory.getParser(flags[1]);
-				p.setUrl(flags[0]);
-				p.setTags(flags[2], flags[3], flags[4], flags[5], flags[6], flags[7]);
-				p.execute();// Aqui se le diria al parser que meta los datos en la base
-			}
-		} catch (IOException e) {
-			System.err.println("ERROR: Fallo en la entrada de texto");
-			e.printStackTrace();
-		} finally {
-			try {
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		// String line;
+		// try {
+		// line = in.readLine(); //Se consume la linea de ejemplo
+		// while ((line = in.readLine()) != null) {
+		// String[] flags = line.trim().split("@");
+		// Parser p = ParserFactory.getParser(flags[1]);
+		// p.setUrl(flags[0]);
+		// p.setTags(flags[2], flags[3], flags[4], flags[5], flags[6],
+		// flags[7]);
+		// p.execute();// Aqui se le diria al parser que meta los datos en la
+		// base
+		// }
+		// } catch (IOException e) {
+		// System.err.println("ERROR: Fallo en la entrada de texto");
+		// e.printStackTrace();
+		// } finally {
+		// try {
+		// in.close();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		// }
 	}
 }
