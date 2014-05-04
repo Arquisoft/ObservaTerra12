4abccc4cc4c17ef263220e7d083c0a56c499ec84
A-Pato <UO214630@uniovi.es>
2014-05-04 17:20:51 +0200

    Modificacion PropertiesReader

--- ObservaTerra12/app/parser/util/PropertiesRead.java
+++ ObservaTerra12/app/parser/util/PropertiesRead.java
@@ -4,77 +4,87 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class PropertiesRead {
 
-	private final int URL = 0;
-	private final int FORMAT = 1;
-	private final int AREA = 2;
-	private final int INDICATOR = 3;
-	private final int MEASURE = 4;
-	private final int TIME = 5;
-	private final int PROVIDER = 6;
-	private final int SUBMISSION = 7;
+	private final static int URL = 0;
+	private final static int FORMAT = 1;
+	private final static int AREA = 2;
+	private final static int INDICATOR = 3;
+	private final static int MEASURE = 4;
+	private final static int TIME = 5;
+	private final static int PROVIDER = 6;
+	private final static int SUBMISSION = 7;
+	private final static int NUMBER_OF_TAGS = 8;
 
-	private BufferedReader in;
-	private List<FileEntry> entries;
-
-	private int numberOfLines;
+	private static BufferedReader in;
 
 	/**
-	 * Constructor de la clase que recibe la ruta del fichero de propiedades.
-	 * 
 	 * @param propFilePath
-	 *            La ruta del fichero de propiedades desde el que se leera.
+	 *            La ruta del fichero de propiedades.
+	 * @return La URL del archivo a descargar.
 	 */
-	public PropertiesRead(String propFilePath) {
+	public static String getURL(String propFilePath) {
+		setIn(propFilePath);
 		try {
-			this.in = new BufferedReader(new FileReader(propFilePath));
-		} catch (FileNotFoundException e) {
-			System.err
-					.println("ERROR: No se encuentra el archivo de propiedades");
+			readTrush();
+			return extractURLFrom(in.readLine());
+		} catch (IOException e) {
+			System.out
+					.println("ERROR: Fallo en la entrada de la propiedad de una pagina");
 			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				System.out
+						.println("ERROR: Fallo al cerrar el lector de propiedades.");
+				e.printStackTrace();
+			}
 		}
-		this.entries = new ArrayList<FileEntry>();
-		this.numberOfLines = 0;
+		return null;
 	}
 
 	/**
-	 * Metodo que extrae la direccion del recurso y las etiquetas de cada una de
-	 * las entradas del fichero de propiedades y las almacena en una lista.
+	 * @param propFilePath
+	 *            La ruta del fichero de propiedades.
+	 * @return Un objeto FileEntry con la URL y las etiquetas de la entrada.
 	 */
-	public void extractData() {
+	public static FileEntry getEntry(String propFilePath) {
+		setIn(propFilePath);
 		try {
 			readTrush();
-			while (in.ready()) {
-				insertEntry(in.readLine());
-				this.numberOfLines++;
-			}
+			return extractEntryFrom(in.readLine());
 		} catch (IOException e) {
 			System.err
 					.println("ERROR: Fallo en la entrada de la propiedad de una pagina");
 			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				System.out
+						.println("ERROR: Fallo al cerrar el lector de propiedades.");
+				e.printStackTrace();
+			}
 		}
+		return null;
 	}
 
-	private void insertEntry(String line) {
-		String[] tags;
-		
-		tags = line.replaceAll("\\s+", "").split("@"); // Elimina todos los
-														// caracteres basura de
-														// la entrada y separa
-														// la direccion del
-														// recurso y cada una de
-														// las etiquetas.
-		if (tags.length != 8)
-			this.entries.add(null);
-		else
-			this.entries.add(new FileEntry(tags[URL], tags[FORMAT], tags[AREA],
-					tags[INDICATOR], tags[MEASURE], tags[TIME], tags[PROVIDER],
-					tags[SUBMISSION])); // Almacena la entrada ya troceada
+	/**
+	 * Metodo que inicializa el lector con la ruta especificada.
+	 * 
+	 * @param propFilePath
+	 *            La nueva ruta del fichero de propiedades.
+	 */
+	private static void setIn(String propFilePath) {
+		try {
+			in = new BufferedReader(new FileReader(propFilePath));
+		} catch (FileNotFoundException e) {
+			System.out
+					.println("ERROR: No se encuentra el archivo de propiedades");
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -82,110 +92,57 @@ public class PropertiesRead {
 	 * 
 	 * @throws IOException
 	 */
-	private void readTrush() throws IOException {
-		in.readLine();
-		this.numberOfLines++;
-	}
-
-	public List<FileEntry> getEntries() {
-		return Collections.unmodifiableList(entries);
+	private static void readTrush() throws IOException {
+		while (in.ready()
+				&& (in.readLine().replaceAll("\\s+", "").charAt(0) == '#'))
+			in.readLine();
 	}
 
 	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
+	 * Metodo que devuelve una cadena con la URL obteniendola de la cadena
+	 * especificada.
+	 * 
+	 * @param line
+	 *            Linea desde la que se intentara obtener la URL.
+	 * @return Si la cadena se obtuvo devolvera una cadena con ella o null en
+	 *         caso contrario.
+	 */
+	private static String extractURLFrom(String line) {
+		String[] tags;
+
+		tags = line.replaceAll("\\s+", "").split("@"); // Elimina todos los
+														// caracteres basura de
+														// la entrada y separa
+														// la direccion del
+														// recurso y cada una de
+														// las etiquetas.
+		if (tags.length != NUMBER_OF_TAGS)
+			return null;
+		return tags[URL];
+	}
+
+	/**
+	 * Metodo que extrae una URL y unas etiquetas desde una cadena y devuelve un
+	 * objeto FileEntry.
+	 * 
+	 * @param line
+	 *            Cadena desde donde se van a extraer los datos.
 	 * @return Un objeto FileEntry con la URL y las etiquetas de la entrada.
 	 */
-	public FileEntry getSiteProperties(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex);
-	}
+	private static FileEntry extractEntryFrom(String line) {
+		String[] tags;
 
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La URL del recurso.
-	 */
-	public String getURL(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getUrl();
-	}
+		tags = line.replaceAll("\\s+", "").split("@"); // Elimina todos los
+														// caracteres basura de
+														// la entrada y separa
+														// la direccion del
+														// recurso y cada una de
+														// las etiquetas.
+		if (tags.length != NUMBER_OF_TAGS)
+			return null;
+		return new FileEntry(tags[URL], tags[FORMAT], tags[AREA],
+				tags[INDICATOR], tags[MEASURE], tags[TIME], tags[PROVIDER],
+				tags[SUBMISSION]);
 
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar al Format.
-	 */
-	public String getFormat(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getFormatTag();
 	}
-
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar al Area.
-	 */
-	public String getArea(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getAreaTag();
-	}
-
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar al Indicator.
-	 */
-	public String getIndicator(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getIndicatorTag();
-	}
-
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar a la Measure.
-	 */
-	public String getMeasure(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getMeasureTag();
-	}
-
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar al Time.
-	 */
-	public String getTime(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getTimeTag();
-	}
-
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar al Provider.
-	 */
-	public String getProvider(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getProviderTag();
-	}
-
-	/**
-	 * @param pageIndex
-	 *            El numero de linea en el fichero de propiedades de la entrada.
-	 * @return La etiqueta que usa el recurso para identificar a la Submission.
-	 */
-	public String getSubmission(int pageIndex) {
-		checkRangeAccess(pageIndex);
-		return this.entries.get(pageIndex).getSubmissionTag();
-	}
-
-	private void checkRangeAccess(int index) {
-		if (index < 2 || this.numberOfLines < index)
-			throw new IndexOutOfBoundsException(
-					"El acceso al recurso ha de estar entre la linea 2 y la "
-							+ this.numberOfLines + ", ambas inclusive");
-	}
-
 }
